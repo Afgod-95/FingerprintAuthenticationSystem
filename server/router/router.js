a2e09531c876227013ef22ebcf3284c890b63465
Afgod-95 <afgod98@gmail.com>
2024-03-08 10:30:52 +0000

    New Commit

--- server/router/router.js
+++ server/router/router.js
@@ -2,9 +2,7 @@ const express = require('express')
 const fingerprintController = require('../controller/student')
 const router = express.Router()
 
-fingerprintController.someAsyncOperation().then(() => {
-    router.post('/api/auth/login', fingerprintController.login);
-    router.post('/api/auth/register', fingerprintController.register);
-  });
+router.post('/api/auth/login', fingerprintController.login)
+router.post('/api/auth/register', fingerprintController.register)
 
 module.exports = router
