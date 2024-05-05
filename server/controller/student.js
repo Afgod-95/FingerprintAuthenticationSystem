def29a13c58dc8ff288d7a51064fb172a2d15d09
Afgod-95 <afgod98@gmail.com>
2024-05-05 14:28:23 +0000

    New hell

--- server/controller/student.js
+++ server/controller/student.js
@@ -47,7 +47,11 @@ const storage = multer.diskStorage({
   }
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 10 * 1024 * 1024 }, // 10 MB limit
+}).single('profilePic[0][image]');
+
 
 const fingerprintController = {
   register: async (req, res) => {
