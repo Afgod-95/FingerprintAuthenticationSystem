74b7a8444ee3a6f755646df6ff18088f07ea5b7b
Afgod-95 <afgod98@gmail.com>
2024-07-14 20:33:03 +0000

    Added Forgot Password and Password Reset

--- server/router/router.js
+++ server/router/router.js
@@ -5,7 +5,7 @@ const router = express.Router()
 router.post('/api/auth/login', fingerprintController.login)
 router.post('/api/auth/register', fingerprintController.register)
 router.get('/api/student/:id', fingerprintController.getStudentID)
-router.get('/api/auth/forgot-password', fingerprintController.sendResetPassword)
-router.get('/api/auth/reset-password', fingerprintController.sendResetPassword)
+router.post('/api/auth/forgot-password', fingerprintController.sendResetPassword)
+router.post('/api/auth/reset-password', fingerprintController.sendResetPassword)
 
 module.exports = router
